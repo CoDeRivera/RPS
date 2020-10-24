6757db4fc8bc6cce972af27d2ed63a79a61ed520
SHaDoWCoDe 64 <shadowcode64@gmail.com>
2020-10-23 20:26:27 -0400

    Created playTable.js

---
+++ src/components/playTable.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import {Container} from 'react-bootstrap'
+
+
+const PlayTable = () => {
+    return (
+        <Container fluid style={{border:"1px dashed black"}}> 
+            PLAY TABLE!
+        </Container>
+    )
+}
+
+
+
+export default PlayTable

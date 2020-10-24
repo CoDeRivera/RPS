ec4a2e1040e361d95e09bd562f015be3c94c4306
SHaDoWCoDe 64 <shadowcode64@gmail.com>
2020-10-23 20:28:22 -0400

    Created playTable.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Navigation from './components/navbar'
+import PlayTable from './components/playTable'
 
   const App = () => {
     return (
       <div>
         <Navigation />
+        <PlayTable />
       </div>
     )
 }

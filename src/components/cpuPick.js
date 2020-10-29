edfe74a5daa625ee6bf5b435f9bbfccd356198cd
SHaDoWCoDe 64 <shadowcode64@gmail.com>
2020-10-29 19:36:58 -0400

    Added cpuPick component to randomly generate a move for pc each round

---
+++ src/components/cpuPick.js
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from 'react'
+
+
+const CpuPick = (props) => {
+    //Maintains state of random hand gesture generated for each round
+    const [cpuPicked, setCpuPicked] = useState('cpu_rock.jpg')  //Will need to be set to null when loading playTable
+  
+    //Runs once component first mounts (For Testing Purposes Only)
+    useEffect(() => cpuTurn()
+    ,[])
+
+    //Runs when new round has started
+    useEffect(() => cpuTurn()
+    , [props.roundCount])
+
+    //Generates a random hand position for CPU's Turn
+    const cpuTurn = () => {
+        //Random pick by CPU
+        let cpuPick = Math.floor(Math.random() * Math.floor(3));
+
+        //Assigns a hand gesture to random number
+        switch(cpuPick){
+            case 0 : setCpuPicked('cpu_rock.jpg')
+                break;
+            case 1 : setCpuPicked('cpu_paper.jpg')
+                break;
+            default : setCpuPicked('cpu_scissors.jpg')
+ 
+        } 
+        return cpuPicked
+    }
+
+    return(
+        <div>             
+            <img alt="CPU's choice" src={`https://res.cloudinary.com/dd6mwtyrj/image/upload/v1603509718/RPS/${cpuPicked}`} />
+        </div>
+    )
+}
+
+export default CpuPick
+
+//cpu_rock = https://res.cloudinary.com/dd6mwtyrj/image/upload/v1603509718/RPS/cpu_rock.png
+//cpu_paper = https://res.cloudinary.com/dd6mwtyrj/image/upload/v1603510054/RPS/cpu_paper.jpg
+//cpu_scissor = https://res.cloudinary.com/dd6mwtyrj/image/upload/v1603509720/RPS/cpu_scissors.jpg
--- src/components/playTable.js
+++ src/components/playTable.js
@@ -1,27 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
+import CpuPick from './cpuPick'
 
-import {Container, Row, Col} from 'react-bootstrap'
+import {Container, Row, Col, Button} from 'react-bootstrap'
 import '../css/playTableStyles.css'
 
 
 const PlayTable = () => {
+    const [round, setRound] = useState(1)
+ 
+    const handleNextRound = (cpuTurn) => {
+        //Will start next round
+        setRound(round + 1)
+        // console.log('next round')
+    }
+    
+
     return (
         <Container fluid > 
+        <h1>Round: {round}</h1>
             <Container id='min-contain'>
                 <Row >
                     <Col id='user' className='play-area'>
                         <img alt='User_Rock' src='https://res.cloudinary.com/dd6mwtyrj/image/upload/v1603509987/RPS/user_rock.png' />
-                        {/* <img alt='User_Paper' src='https://res.cloudinary.com/dd6mwtyrj/image/upload/v1603509987/RPS/user_paper.png' />
-                        <img alt='User_Scissors' src='https://res.cloudinary.com/dd6mwtyrj/image/upload/v1603509987/RPS/user_scissors.png' /> */}
+                        {/* <img alt='User_Paper' src='https://res.cloudinary.com/dd6mwtyrj/image/upload/v1603509987/RPS/user_paper.png' /> */}
+                        {/*<img alt='User_Scissors' src='https://res.cloudinary.com/dd6mwtyrj/image/upload/v1603509987/RPS/user_scissors.png' /> */}
                     </Col>
                     <Col  id='cpu' className='play-area'>
-                        <img alt='CPU_Scissors' src='https://res.cloudinary.com/dd6mwtyrj/image/upload/v1603509718/RPS/cpu_scissors.png' /><br />
-                        {/* <img alt='CPU_Rock' src='https://res.cloudinary.com/dd6mwtyrj/image/upload/v1603509718/RPS/cpu_rock.png' />
-                        <img alt='CPU_Paper' src='https://res.cloudinary.com/dd6mwtyrj/image/upload/v1603509718/RPS/cpu_paper.png' /> */}
-                        
+                       <CpuPick roundCount={round} />
                     </Col>
                 </Row>
             </Container>
+            <center><Button variant='success' onClick={()=> handleNextRound()}>Next Round</Button></center>
         </Container>
     )
 }
@@ -36,6 +45,3 @@ export default PlayTable
 //user_paper = https://res.cloudinary.com/dd6mwtyrj/image/upload/v1603509830/RPS/user_paper.jpg
 //user_scissor = https://res.cloudinary.com/dd6mwtyrj/image/upload/v1603509964/RPS/user_scissor.jpg
 
-//cpu_rock = https://res.cloudinary.com/dd6mwtyrj/image/upload/v1603509718/RPS/cpu_rock.png
-//cpu_paper = https://res.cloudinary.com/dd6mwtyrj/image/upload/v1603510054/RPS/cpu_paper.jpg
-//cpu_scissor = https://res.cloudinary.com/dd6mwtyrj/image/upload/v1603509720/RPS/cpu_scissors.jpg
